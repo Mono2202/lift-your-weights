f32d04b38df6171961ab32df5bcc38b7dd596d7c
Mono <monocode22@gmail.com>
2025-05-19 00:31:16 +0300

    More messages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+import itertools
 
 from datetime import datetime
 from dotenv import load_dotenv
@@ -11,19 +12,26 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = "6518216142"
-MESSAGE="Ligma"
+MESSAGES = [
+    "BICEP CURL 💪",
+    "PUSHUPS 🫸",
+    "Maybe something different??? Maybe... Shoulders...? Maybe... Legs????? 🤔🤔🤔"
+]
 
 START_HOUR = 10
 END_HOUR = 23
 SKIPPED_DAYS = [4, 5]
 
 async def periodic_sender(bot: Bot):
+    messages_cycler = itertools.cycle(MESSAGES)
+
     while True:
         now = datetime.now()
         weekday = now.weekday()
         #if weekday not in SKIPPED_DAYS and START_HOUR <= now.hour < END_HOUR:
-        await bot.send_message(chat_id=CHAT_ID, text=MESSAGE)
-        await asyncio.sleep(60 * 30)  # Wait 30 minutes
+        await bot.send_message(chat_id=CHAT_ID, text=next(messages_cycler))
+        #await asyncio.sleep(60 * 30)  # Wait 30 minutes
+        await asyncio.sleep(10)  # Wait 30 minutes
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
